e855285127455cbae62e6af5cda05beec20cbab6
Bgoon <isg1153@naver.com>
2019-04-08 23:52:35 +0900

    파일 추가

---
+++ WinsCode/WinsCode/Views/Windows/TerminalWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinsCode {
+	/// <summary>
+	/// MainWindow.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class TerminalWindow : Window {
+
+		public TerminalWindow() {
+			InitializeComponent();
+			Loaded += OnLoaded;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e) {
+			Left = 0d;
+			Top = 0d;
+			Width = 1920d;
+
+			OutputText.Text = "결과 출력 디스플레이\nExecute...\n존-새 밥오.";
+		}
+	}
+}
